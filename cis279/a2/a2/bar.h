108eafa57ff8093049e9f687855987e74cd900ae
Mark Davis <mark@yingliamericas.com>
2018-03-11 19:27:13 -0700

    straight lift from sample code

---
+++ cis279/a2/a2/bar.h
@@ -0,0 +1,28 @@
+//
+//  Assignment 2
+//
+//  bar.h
+//
+
+#ifndef bar_h
+#define bar_h
+
+class SoftwareGurusBar {
+    public:
+        //
+        // try with 50 chairs, then try with 40, 60, ...
+        // in order to find out “optimal” profit prospects
+        //
+        SoftwareGurusBar()
+        : freeChairs(50), profit(0.0) { }
+        bool canSeat (unsigned int numberOfPeople);  // slide 12
+        void order(unsigned int beerType);           // slide 13
+        void leave(unsigned int numberOfPeople);     // slide 13
+
+        unsigned int freeChairs;
+        double profit;
+};
+
+extern SoftwareGurusBar theBar;
+
+#endif /* bar_h */
